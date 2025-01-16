7f7c5d91c2cefd59bb183de2681acb6e9ad4ff3f
MMAGES2006 <22170@portalcedi.edu.mx>
2025-01-15 20:58:46 -0600

    Paginas creadas

--- App1/src/app/app-routing.module.ts
+++ App1/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {
@@ -10,6 +11,10 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
   },
 ];
 
